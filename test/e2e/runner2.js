a910ecfcc08dea279c6401fc4bcda6413a15b4d4
nguyenan <annguyen_qh@yahoo.com>
2018-09-05 16:26:26 +0700

    Update runner2.js

--- test/e2e/runner2.js
+++ test/e2e/runner2.js
@@ -23,11 +23,7 @@ runner.on('error', function (err) {
   server.close()
   throw err
 })
-function myFunction(p1, p2) {
-    return p1 * p2; 
-}
 // An edited
 // An edited 17:19 Thursday, July 5, 2018 (GMT+7)
 // updated
 // delete some lines
-// add some functions
